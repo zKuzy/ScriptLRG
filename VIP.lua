d9ebc9be83c0e18c9b086406be02637cc1b2a087
zKuzy <113568489+zKuzy@users.noreply.github.com>
2023-01-03 13:37:56 +0700

    Update VIP.lua

--- VIP.lua
+++ VIP.lua
@@ -1,1 +1,10 @@
-gg.makeRequest(\"https://raw.githubusercontent.com/zKuzy/ScriptLRG/main/VIP.lua\").content
+--GameGuardian makes a internet request
+local API = gg.makeRequest("https://raw.githubusercontent.com/zKuzy/ScriptLRG/main/VIP.lua").content
+
+--Checks if you granted access. if true then it will procceed to load the code inside of the pastebin
+if API then
+  pcall(load(API))
+ else
+  gg["alert"]("Error You have not granted internet access!")
+  os.exit()
+end
